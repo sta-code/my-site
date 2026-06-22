64f9dd9f4ec001278215274c39be91f80a0d1051
Staline Gbedji <stalinegbedji405@gmail.com>
2026-06-22 17:21:29 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+<script>
+new Typed("#typed", {
+
+strings: [
+"Développeur Full Stack",
+"Étudiant à l'IFRI",
+"Passionné d'IA",
+"Développeur Django"
+],
+
+typeSpeed:70,
+backSpeed:50,
+loop:true
+
+});
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 100
+    },
+    color: {
+      value: "#0d6efd"
+    }
+  }
+});
+</script>
